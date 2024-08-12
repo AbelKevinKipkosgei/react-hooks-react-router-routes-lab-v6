aca554aa220605c5436643861d85a9e14d6d7eb0
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-08-12 17:18:10 +0300

    Completed and corrected the react router lab.

--- src/pages/Directors.js
+++ src/pages/Directors.js
@@ -1,16 +1,38 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Directors() {
+  const [directors, setDirectors] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/directors")
+      .then((r) => r.json())
+      .then((data) => setDirectors(data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Director info here! */}
+        <h1>Directors Page</h1>
+        <article>
+          {directors.map((director) => (
+            <div key={director.id}>
+              <h2 key={director.id}>{director.name}</h2>
+              <ul>
+                {director.movies.map((directorMovie, index) => (
+                  <li key={index}>{directorMovie}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </article>
       </main>
     </>
   );
-};
+}
 
 export default Directors;
